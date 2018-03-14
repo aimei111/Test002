f84f75d5f0b71e5f93c1416a09cb3a5dc3958330
LGSC <574677347@qq.com>
2018-03-14 15:15:38 +0800

    上传注释

--- NewGitTest002/ViewController.m
+++ NewGitTest002/ViewController.m
@@ -17,7 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"1234456");
+    NSLog(@"1234456"); //这是注释
 }
 
 
