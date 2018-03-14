accedfc6d17509a3d4837e914478c1e52a96a9e3
LGSC <574677347@qq.com>
2018-03-14 11:58:43 +0800

    Initial Commit

---
+++ NewGitTest002/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewGitTest002
+//
+//  Created by LGSC on 2018/3/14.
+//  Copyright © 2018年 LGSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewGitTest002/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewGitTest002
+//
+//  Created by LGSC on 2018/3/14.
+//  Copyright © 2018年 LGSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
